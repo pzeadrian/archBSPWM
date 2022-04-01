f51bd0d4cf90c8290965f46d64288aaca7736ec2
pzeadrian <pzeadrian@outlook.com>
2022-03-31 19:00:24 -0500

    Updating configs

---
+++ BSPWM/bspwm/monitorsVolume.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+r=$(echo -e "Internal\nExternal\nBluetooth" | dmenu -i -p "Select sound output > ") #-fn "Cantarell-14" -nf '#ffb52a' -sb '#ffb52a' -sf '#222')
+
+case "$r" in
+    Internal)  pactl set-default-sink alsa_output.pci-0000_03_00.6.analog-stereo ;;
+    External) pactl set-default-sink alsa_output.pci-0000_03_00.1.hdmi-stereo ;;
+    Bluetooth) pactl set-default-sink bluez_sink.00_00_00_00_8F_61.a2dp_sink ;;
+    *) echo "Not found $r (Internal|External)" ;;
+esac
