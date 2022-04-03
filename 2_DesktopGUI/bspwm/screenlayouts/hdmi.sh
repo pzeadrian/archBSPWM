947be941a114533df17385233ab7751e1119aacc
pzeadrian <pzeadrian@outlook.com>
2022-04-03 13:08:58 -0500

    Updating configs

--- 2_DesktopGUI/bspwm/screenlayouts/hdmi.sh
+++ 2_DesktopGUI/bspwm/screenlayouts/hdmi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
 
 sleep 1
 
