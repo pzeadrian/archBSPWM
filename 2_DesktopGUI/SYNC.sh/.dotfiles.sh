f7ef3fade16e6cc5004c38f0b791bf8dcc0526cc
pzeadrian <pzeadrian@outlook.com>
2022-04-07 19:28:12 -0500

    Updating configs

--- 2_DesktopGUI/SYNC.sh/.dotfiles.sh
+++ 2_DesktopGUI/SYNC.sh/.dotfiles.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-cp -R $HOME/.config/alacritty $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp -R $HOME/.config/bspwm $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp -R $HOME/.config/init_session $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp -R $HOME/.config/polybar $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp -R $HOME/.config/sxhkd $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp -R $HOME/.config/X11 $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp -R $HOME/.config/zsh $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp -R $HOME/.config/ranger $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp -R $HOME/.config/picom $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp -R $HOME/.config/rofi $HOME/Documents/dotfilesLinux/2_DesktopGUI
-cp $HOME/.dotfiles.sh $HOME/Documents/dotfilesLinux/2_DesktopGUI/SYNC.sh
-cp $HOME/.neovim.sh $HOME/Documents/dotfilesLinux/2_DesktopGUI/SYNC.sh
+cp -R $HOME/.config/alacritty $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp -R $HOME/.config/bspwm $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp -R $HOME/.config/init_session $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp -R $HOME/.config/polybar $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp -R $HOME/.config/sxhkd $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp -R $HOME/.config/X11 $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp -R $HOME/.config/zsh $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp -R $HOME/.config/ranger $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp -R $HOME/.config/picom $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp -R $HOME/.config/rofi $HOME/Documents/dotfilesArch/2_DesktopGUI
+cp $HOME/.dotfiles.sh $HOME/Documents/dotfilesArch/2_DesktopGUI/SYNC.sh
+cp $HOME/.neovim.sh $HOME/Documents/dotfilesArch/2_DesktopGUI/SYNC.sh
 
-cd $HOME/Documents/dotfilesLinux
+cd $HOME/Documents/dotfilesArch
 git add -A
 git commit -m "Updating configs"
 git push
